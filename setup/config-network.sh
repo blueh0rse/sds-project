a23cf29072ca96989908c385954ef4f08b260696
Alexandru Dumitru Maroz <alexandru666@outlook.es>
2024-05-07 12:49:13 -0700

    Working Snort #1

--- setup/config-network.sh
+++ setup/config-network.sh
@@ -5,7 +5,10 @@ workdir=~/sds/sds-project
 cd $workdir
 
 # Setup snort
-sudo ovs-vsctl add-port s1 s1-snort
-sudo snort -i s1-snort -A unsock -l /tmp -c /etc/snort/snort.conf -q -D > /dev/null 2>&1
+# sudo killall -s SIGKILL snort
+sudo cp $workdir/config/snort.conf /etc/snort/snort.conf
+sudo cp $workdir/config/SDS-project_SnortRules.rules /etc/snort/rules/SDS-project_SnortRules.rules
+# sudo ovs-vsctl add-port s2 s1-snort
+# sudo snort -i s1-snort -A unsock -l /tmp -c /etc/snort/snort.conf -q -D > /dev/null 2>&1
 cd $workdir/tools/ryu/
-sudo ryu-manager ryu/app/simple_switch_snort.py
+sudo ryu-manager $workdir/network/custom_switch.py
